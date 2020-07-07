fd5e514592952a723bf31c9eab9cd4a7ee07305e
JamesGaier <james.gaier003@gmail.com>
2020-07-07 13:29:12 -0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, console.log(`Server starting on ${PORT}...`));
