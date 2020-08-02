b06ed4992160d3dd84a0d4ccb4c0d4c414575671
JamesGaier <james.gaier003@gmail.com>
2020-08-02 13:25:39 -0700

    deploy

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 const db = require('./models');
 const controllers = require('./controllers');
 const routes = require('./routes');
